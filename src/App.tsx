4eebae2297a722777f3589fd3e657b4c9de209be
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2023-12-13 15:55:39 +0500

    remove extra required code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './views/Home'
 import AddPosts from './views/AddPosts'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
