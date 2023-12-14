fbfc77e77ff59c5b3f8d325c434e24e2c4615607
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2023-12-14 20:25:11 +0500

    create left side heading

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -6,8 +6,8 @@ const Home = (): JSX.Element => {
     const posts = useSelector(getPosts);
     const postComponent: React.ReactNode = posts.map(post => (
         <div className="p-3" key={post.id}>
-            <div className="border-2 border-gray-100 p-4 rounded-md">
-                <h1 className="text-2xl font-bold underline">{post.title}</h1>
+            <div className="border-2 border-gray-300 bg-gray-500 p-4 rounded-lg shadow-lg shadow-gray-700">
+                <h1 className="text-2xl font-bold">{post.title}</h1>
                 <div className="py-2 text-justify">
                     <p>{post.body}
                     </p>
@@ -19,8 +19,8 @@ const Home = (): JSX.Element => {
     ))
     return (
         <div className="container m-auto p-4">
-            <div className="pb-10 flex flex-wrap justify-end">
-                <Link to={"/add"} className="p-2 border-white border-2 rounded-sm bg-blue-500">Add Post</Link>
+            <div className="pb-10 flex flex-wrap justify-center md:justify-between items-center">
+                <h2 className="pl-3 text-xl text-center font-semibold w-full md:w-auto py-2 md:py-0">Your Posts</h2><Link to={"/add"} className="p-2 border-white border-2 rounded-sm bg-blue-500">Add New Post</Link>
             </div>
             {postComponent}
         </div>
