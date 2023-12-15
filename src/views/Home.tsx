bd33bd728cd739fe24fef5debeacd7b2c980a545
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2023-12-15 13:04:10 +0500

    change the card bg-color

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -6,7 +6,7 @@ const Home = (): JSX.Element => {
     const posts = useSelector(getPosts);
     const postComponent: React.ReactNode = posts.map(post => (
         <div className="p-3" key={post.id}>
-            <div className="border-2 border-gray-300 bg-gray-500 p-4 rounded-lg shadow-lg shadow-gray-700">
+            <div className="border-2 text-[#111111] border-gray-300 bg-[#e9e3dc] p-4 rounded-lg shadow-lg shadow-gray-300">
                 <h1 className="text-2xl font-bold">{post.title}</h1>
                 <div className="py-2 text-justify">
                     <p>{post.body}
@@ -18,9 +18,9 @@ const Home = (): JSX.Element => {
 
     ))
     return (
-        <div className="container m-auto p-4">
+        <div className="container lg:max-w-[1200px] m-auto p-4">
             <div className="pb-10 flex flex-wrap justify-center md:justify-between items-center">
-                <h2 className="pl-3 text-xl text-center font-semibold w-full md:w-auto py-2 md:py-0">Your Posts</h2><Link to={"/add"} className="p-2 border-white border-2 rounded-sm bg-blue-500">Add New Post</Link>
+                <h2 className="pl-3 text-xl text-center font-semibold w-full md:w-auto py-2 md:py-0">Your Posts</h2><Link to={"/add"} className="p-2 border-gray-300 hover:shadow-lg shadow-gray-300 border-2 rounded-md bg-[#e9e3dc]">Add New Post</Link>
             </div>
             {postComponent}
         </div>
